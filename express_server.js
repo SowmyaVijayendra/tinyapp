6bfffb3db57cd44722a83a57699aa22ea73fcb95
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-04-28 07:22:38 -0400

    added end point to return a HTML content

--- express_server.js
+++ express_server.js
@@ -6,12 +6,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//to handle home page by returning a text
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+//to handle another endpoint to return json object 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+// end point to return HTML content
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
