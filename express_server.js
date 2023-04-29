c2e1d77f0cc7c51051bd4fe4b6e9113df882094c
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-04-29 07:41:37 -0400

    added urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -16,6 +16,10 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
+  res.render("urls_show", templateVars);
+});
 // end point to return HTML content
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
