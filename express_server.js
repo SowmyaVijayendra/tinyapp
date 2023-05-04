9e6a7dc6d09c9c06a249ddea7b097194bcb61d5b
Sowmya Vijayendra <sowmyamv15@gmail.com>
2023-05-04 16:58:37 -0400

    implemented login and register get handlers

--- express_server.js
+++ express_server.js
@@ -39,8 +39,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {  user : users[req.cookies["user_id"]], id: req.params.id, longURL: urlDatabase.id };
   res.render("urls_show", templateVars);
 });
-app.get("/urls",(req,res) => {
-  console.log(req.cookies["username"]);
+app.get("/urls",(req,res) => {  
   const templateVars = {   user : users[req.cookies["user_id"]], urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
@@ -55,37 +54,34 @@ app.get("/u/:id", (req, res) => { // get request following redirect from POST
    const longURL = urlDatabase[req.params.id];
   res.redirect(longURL); // redirect to long url of the short url
 });
-app.post("/urls/:id/delete", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console  
+app.post("/urls/:id/delete", (req, res) => { 
  delete urlDatabase[req.params.id]; //delete from the DB
   res.redirect("/urls"); // Redirect to index page
 });
-app.post("/urls/:id/edit", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console  
+app.post("/urls/:id/edit", (req, res) => {    
   res.redirect(`/urls/${req.params.id}`); // Redirect to show page
 });
-app.post("/urls/:id", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console 
-  console.log(req.params.id) ;
+app.post("/urls/:id", (req, res) => { 
   urlDatabase[req.params.id] = req.body.newLongURL;
   res.redirect("/urls"); // Redirect to index page
 });
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
-  res.redirect("/urls"); // Redirect to index page
-});
+
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls"); // Redirect to index page
 });
 app.get("/register", (req, res) => { // get request for registering
   res.render("urls_register");
 });
 app.post("/register" ,(req,res) => { //handler for register
- if(!req.body.email || !req.body.password || getUserByEmail(req.body.email)){// if either or both email aor pawd is empty, return 400 status
+ if(!req.body.email || !req.body.password ){// if either or both email aor pawd is empty, return 400 status
 res.statusMessage= "Username or Password is empty";
 res.status(400).end();
  } 
+ else if(getUserByEmail(req.body.email)){//if username already exists in the DB, return 400 error status
+res.statusMessage= "Username already exists";
+res.status(400).end();
+ }
  let user = {};
  user["id"] = generateRandomString();
  user["email"] = req.body.email;
@@ -94,7 +90,18 @@ res.status(400).end();
  console.log(users);
  res.cookie("user_id",user["id"]);
  res.redirect("/urls"); // Redirect to index page
-})
+});
+app.get("/login", (req, res) => { // get request for login page
+  res.render("urls_login");
+});
+app.post("/login" ,(req,res) => { //handler for login
+  if(!req.body.email || !req.body.password ){// if either or both email aor pawd is empty, return 400 status
+ res.statusMessage= "Username or Password is empty";
+ res.status(400).end();
+  }  
+  res.cookie("user_id",user["id"]);
+  res.redirect("/urls"); // Redirect to index page
+ });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
